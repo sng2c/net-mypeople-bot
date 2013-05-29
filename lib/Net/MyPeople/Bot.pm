e13d3c11caf3b1ea795baf79b09cedbd18b2259e
khs <sng2c@sng2c.vps.phps.kr>
2013-05-29 14:55:00 +0900

    fix bug

--- lib/Net/MyPeople/Bot.pm
+++ lib/Net/MyPeople/Bot.pm
@@ -2,12 +2,14 @@ package Net::MyPeople::Bot;
 use 5.010;
 use strict;
 use warnings;
-use utf8;
 use Moose;
 use Data::Dumper;
 use LWP::UserAgent;
+use LWP::Protocol::https;
 use HTTP::Request::Common;
 use JSON;
+use Data::Printer;
+use URI::Escape;
 
 # ABSTRACT: Implements MyPeople-Bot.
 
@@ -37,9 +39,9 @@ use JSON;
 			callback( $action, $buddyId, $groupId, $content );
 		}
 	);
-
 	sub callback{
 		my ($action, $buddyId, $groupId, $content ) = @_;
+		p @_;
 		if   ( $action eq 'addBuddy' ){ # when someone add this bot as a buddy.
 		
 			my $buddy = $bot->buddy($buddyId); # hashref
@@ -48,28 +50,40 @@ use JSON;
 
 		}
 		elsif( $action eq 'sendFromMessage' ){ # when someone send a message to this bot.
-		
-		my $res = $bot->send($buddyId, "(ECHO) $content");
-		
+			my @res = $bot->send($buddyId, "$content");
 		}
 		elsif( $action eq 'createGroup' ){ # when this bot invited to a group chat channel.
 		
-		my $res = $bot->sendGroup($groupId, 'Nice to meet you, guys.');
+			my $res = $bot->groupSend($groupId, 'Nice to meet you, guys.');
 			# CONTENT
+			# [
+			# 	{"buddyId":"BU_ey3aPnSCpzx3ccwqidwdfg00","isBot":"Y","name":"testbot","photoId":"myp_pub:51A586C2074DB00010"},
+			# 	{"buddyId":"BU_ey3aPnSCpzx3ccwqidwdfg00","isBot":"Y","name":"testbot","photoId":"myp_pub:51A586C2074DB00010"}
+			# ]
 		
 		}
-		elsif( $action eq 'inviteToGroup' ){ # when someone in a group chat channel invites other buddy to the channel.
+		elsif( $action eq 'inviteToGroup' ){ # when someone in a group chat channel invites user to the channel.
 			
-			my $buddy = $bot->buddy($buddyId); # hashref
-			my $buddy_name = $buddy->{buddys}->{name};
+			my $buddy_name = $content->[0]->{name};
+			my $is_bot = $content->[0]->{is_bot} eq 'Y';
+			
 			# CONTENT
-			my $res = $bot->sendGroup($groupId, 'Can you introduce that buddy?');
+			# [
+			# 	{"buddyId":"BU_ey3aPnSCpzx3ccwqidwdfg00","isBot":"Y","name":"testbot","photoId":"myp_pub:51A586C2074DB00010"}
+			# ]
+			
+			if( $is_bot ){ # bot self
+				my $res = $bot->groupSend($groupId, 'Nide to meet you, guys');
+			}
 		
+			else{ # other guy
+				my $res = $bot->groupSend($groupId, "$buddy_name, Can you introduce your self?");
+			}
 		}
 		elsif( $action eq 'exitFromGroup' ){ # when someone in a group chat channel leaves.
 
 			my $buddy = $bot->buddy($buddyId); # hashref
-			my $buddy_name = $buddy->{buddys}->{name};
+			my $buddy_name = $buddy->{buddys}->[0]->{name};
 			my $res = $bot->sendGroup($groupId, "I'll miss $buddy_name ...");
 
 		}
@@ -79,11 +93,12 @@ use JSON;
 				$res = $bot->groupExit($groupId);
 			}
 			else{
-				my $res = $bot->sendGroup($groupId, "(GROUP_ECHO) $content");
+				my $res = $bot->groupSend($groupId, "(GROUP_ECHO) $content");
 			}
 		}
 	}
 
+	$httpd->run;
 =cut
 
 has apikey=>(
@@ -111,7 +126,13 @@ sub _call_file {
 	my $self = shift;
 	my ($apiurl, $param, $path) = @_;
 	$param->{apikey} = $self->apikey;
-	my $res = $self->ua->post( $apiurl, $param );
+	foreach my $k (keys %{$param}){
+		$param->{$k} = uri_escape($param->{$k});
+	}
+
+	my $req = POST( $apiurl, Content=>$param );
+	my $res = $self->ua->request( $req );
+
 	if( $res->is_success ){
 		$path =~ s@[\\/]$@@;
 		my $filepath;
@@ -135,13 +156,18 @@ sub _call_multipart {
 	my $self = shift;
 	my ($apiurl, $param) = @_;
 	$param->{apikey} = $self->apikey;
+	foreach my $k (keys %{$param}){
+		$param->{$k} = uri_escape($param->{$k});
+	}
 
-	
-	my $res = $self->ua->post( $apiurl, 
+	my $req = POST(	$apiurl, 
 		Content_Type => 'form-data',
-		Content => [
-			%{$param}
-		]);
+		Content => $param
+		);
+#	print $req->as_string;
+
+	my $res = $self->ua->request($req);
+
 	if( $res->is_success ){
 		return from_json( $res->content );
 	}
@@ -153,12 +179,18 @@ sub _call {
 	my $self = shift;
 	my ($apiurl, $param) = @_;
 	$param->{apikey} = $self->apikey;
-	my $res = $self->ua->post( $apiurl, $param );
+	foreach my $k (keys %{$param}){
+		$param->{$k} = uri_escape($param->{$k});
+	}
+
+	my $req = POST( $apiurl, Content=>$param );
+	my $res = $self->ua->request( $req );
+	
 	if( $res->is_success ){
 		return from_json( $res->content );
 	}
 	else{
-		return undef;
+		return undef, $res;
 	}
 }
 
@@ -178,9 +210,7 @@ sub send{
 	my $self = shift;
 	my ($buddyId, $content, $attach) = @_;
 	if( $attach && -f $attach ){
-		$attach =~ m@([^\\/]+)$@;
-		my $filename = $1;
-		return $self->_call_multipart($API_SEND, {buddyId=>$buddyId, attach=>[$attach,$filename]} );
+		return $self->_call_multipart($API_SEND, {buddyId=>$buddyId, attach=>[$attach]} );
 	}
 	else{
 		return $self->_call($API_SEND, {buddyId=>$buddyId, content=>$content} );
@@ -191,9 +221,7 @@ sub groupSend{
 	my $self = shift;
 	my ($groupId, $content, $attach) = @_;
 	if( $attach && -f $attach ){
-		$attach =~ m@([^\\/]+)$@;
-		my $filename = $1;
-		return $self->_call_multipart($API_GROUP_SEND, {groupId=>$groupId, attach=>[$attach,$filename]} );
+		return $self->_call_multipart($API_GROUP_SEND, {groupId=>$groupId, attach=>[$attach]} );
 	}
 	else{
 		return $self->_call($API_GROUP_SEND, {groupId=>$groupId, content=>$content} );
